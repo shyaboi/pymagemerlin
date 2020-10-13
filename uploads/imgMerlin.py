c75b640c8643c1edac8b3c0a191437f546b4a14d
Shyaboi <ian.shane.sears@gmail.com>
2020-10-12 23:01:41 -0700

    moving uploaded file to originals folder, and compressing and moving to compressed folder smoothly

--- uploads/imgMerlin.py
+++ uploads/imgMerlin.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import time
+import time, shutil
 from pathlib import Path
 
 # dir(Image)
@@ -8,5 +8,6 @@ def imgSmush(importPic, qual):
     img = str(Path("uploads/"+importPic))
     print(img)
     picture = Image.open(img)
-    picture.save(f"static/compressed/compressed{qual}" + importPic ,optimize=True,quality=qual)
-    importPic.save("static/originials/original" + importPic ,optimize=True,quality=100)
+    picture.save(f"static/compressed/compressed{qual}{importPic}", optimize=True,quality=qual)
+    # importPic.save("static/originials/original" + importPic ,optimize=True,quality=100)
+    shutil.move(img, f'static/originials/{importPic}')
